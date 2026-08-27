93c067659c996115f49be690b2c5dfdfeafc4f70
Panoch Stitsart <p_noch@msn.com>
2026-08-27 18:35:44 +0700

    fix: guard fire-and-forget history upload with .catch

--- lib/background-tasks/refresh-listing.ts
+++ lib/background-tasks/refresh-listing.ts
@@ -34,7 +34,7 @@ export async function refreshListing(
         listing.distributorId,
         product.modelNumber,
         listing.priceHistory,
-      );
+      ).catch(() => {});
     }
     return {
       ...listing,
